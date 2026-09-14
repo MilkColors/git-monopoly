6e4dd1a418782eaad469d7b090a7cab18d3b9b7b
MilkColors <128tcarney@frhsd.com>
2026-09-14 10:54:22 -0400

    Arraylist

--- src/ItemInfo.java
+++ src/ItemInfo.java
@@ -1,25 +1,12 @@
 import java.util.*;
-public class ItemInfo {
-    private String itemName;
-    private double cost;
-    private boolean isAvailable;
-    public ItemInfo(String itemName, double cost, boolean isAvailable) {
-        this.itemName = itemName;
-        this.cost = cost;
-        this.isAvailable = isAvailable;
-    }
 
-    public String getName() {
-        return  itemName;
-    }
-
-    public double getCost(){
-    return 0.0;
-    }
+public class itemInfo {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the name of the item: ");
-        String item = input.nextLine();
+
+        public boolean isAvailable()
+        {
+            getName.
+        }
     }
 }
