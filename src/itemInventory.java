a08ff78bd06e7477ec5d9ba263390f5438559c68
MilkColors <128tcarney@frhsd.com>
2026-09-14 10:53:11 -0400

    Arraylist

---
+++ src/itemInventory.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class itemInventory {
+    private ArrayList<itemInfo>inventory;
+    public itemInventory() {
+        ArrayList inventory = new ArrayList( );
+        inventory.add(new itemInfo("action figure", ));
+    }
+    public double averageWithinRange(double lower, double upper) {
+
+    }
+}
